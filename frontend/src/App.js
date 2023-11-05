168ca8f35145fc9af2c9ac11f5abd8d072c64100
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-11-05 22:19:15 +0530

    Organised contents of frontend-main branch in frontend folder

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Login from './component/Login/Login'
+import Signup from './component/Signup/Signup'
+
+const App = () => {
+  return (
+    <div>
+      <Login/>
+      <Signup/>
+    </div>
+  )
+}
+
+export default App
